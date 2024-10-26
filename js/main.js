7429740059644bf6e326cba2180578a1264a8212
abdelkarimhajji <hajji.karim.solicode@gmail.com>
2024-10-26 12:11:25 +0100

    push

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
 let eye = document.getElementById('eye');
-let input = document.getElementById('password');
- 
-eye.addEventListener('click', function(){
-    if(eye.classList.contains('fa-eye-slash'))
-    {
-        eye.classList.remove('fa-eye-slash');
-        eye.classList.add('fa-eye');
-        input.type = ''
-    }
-    else
-    {
-        eye.classList.remove('fa-eye');
-        eye.classList.add('fa-eye-slash');
-        input.type = 'password'
-    }
-})
 
+if (eye) {
+    eye.addEventListener('click', function(){
+        if(eye.classList.contains('fa-eye-slash'))
+        {
+            eye.classList.remove('fa-eye-slash');
+            eye.classList.add('fa-eye');
+            input.type = ''
+        }
+        else
+        {
+            eye.classList.remove('fa-eye');
+            eye.classList.add('fa-eye-slash');
+            input.type = 'password'
+        }
+    })
+}
 
 
 // confirmation password
 
-let confirm = document.getElementById('confirm-password');
 
 
-// if(input.value == confirm.value)
+// display poupup email
+
+let button_email = document.getElementById("email");
+let close_ = document.getElementById("close");
+let poupup = document.getElementById("container-poupup")
+if(button_email)
+{
+    button_email.addEventListener('click', function(){
+        poupup.style.display = "block";
+    });
+}
+if(close_)
+{
+    close_.addEventListener('click', function() {
+        poupup.style.display = "none"
+    })
+}
