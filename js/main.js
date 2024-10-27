28e2deaee40dcde3317cd4c4a063edd2f9b84827
abdelkarimhajji <hajji.karim.solicode@gmail.com>
2024-10-27 18:03:35 +0100

    push

--- js/main.js
+++ js/main.js
@@ -23,43 +23,61 @@ if (eye) {
 
 let button_email = document.getElementById("email");
 let close_ = document.getElementById("close");
-let poupup = document.getElementById("container-poupup")
+let poupup = document.getElementById("container-poupup");
+let form = document.querySelector(".container-poupup > .container-form");
 if(button_email)
 {
-    button_email.addEventListener('click', function(){
-    
-        poupup.style.left = 0;
+    button_email.addEventListener('click', function(){    
+        poupup.style.opacity = "1";
+        poupup.style.pointerEvents = "all";
     });
 }
 if(close_)
 {
     close_.addEventListener('click', function() {
-       poupup.style.left = "-2500px";
+        poupup.style.pointerEvents = "none";
+        poupup.style.opacity = "0";
+        validate_message.style.left = "-400px";
+        validate_email.value = "";
     })
 }
 
 
 // display message validate
 
+
+
+
 let validate_message = document.getElementById("container-message");
 let validate_email = document.getElementById("validate-email");
 let send_email = document.getElementById("send-email");
 let close_validate = document.getElementById("close-validate");
+let message_not_valid = document.getElementById("message-not-valid");
 
 
+function isValidEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+}
+
+ 
 if(send_email && validate_message && send_email)
 {
     send_email.addEventListener('click', function(){
-        if(validate_email.value != "")
+        if(isValidEmail(validate_email.value))
         {
-            validate_email.value = ""
+            validate_email.value = "";
             validate_message.style.left = "60px";
+            message_not_valid.style.opacity = "0"
             setTimeout(function() {
                 validate_message.style.left = "-400px";
-            }, 3000);
+            }, 2000);
         }
+        else
+            message_not_valid.style.opacity = "1"
         
     })
+
     close_validate.addEventListener("click", function(){
         validate_message.style.left = "-400px";
     })
